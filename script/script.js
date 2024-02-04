18ac8737ea160f77458c1c51635a6e882ad94c81
Wendell Araujo <wendell170695@gmail.com>
2024-02-04 00:48:10 -0300

    formulário login/new account add

---
+++ script/script.js
@@ -0,0 +1,66 @@
+(function () {
+  verificarLog();
+})();
+
+var count = 0;
+var accessForm = ""
+
+function verificarLog() {
+  var logado = localStorage.getItem("user-log");
+
+  if (logado !== null) {
+    if (logado === true) {
+      $(".user-off").css("display", "none");
+      $(".user-on").css("display", "block");
+    } else {
+      $(".user-off").css("display", "block");
+      $(".user-on").css("display", "none");
+    }
+  } else {
+    $(".user-off").css("display", "block");
+    $(".user-on").css("display", "none");
+    localStorage.setItem("user-log", false);
+  }
+}
+
+function showFormOptions() {
+    if (accessForm === "new-login") {
+        $(".new-account").css("display", "none");
+        $(".new-login").css("display", "block");
+    } else if (accessForm === "new-account") {
+        $(".new-login").css("display", "none");
+        $(".new-account").css("display", "block");
+    }
+}
+
+
+
+$(".menu-icon").on("click", function () {
+  if (count === 0) {
+    $(".sub-menu").css("display", "block");
+    count++;
+  } else {
+    $(".sub-menu").css("display", "none");
+    count = 0;
+  }
+});
+
+$("#accessLogin").on("click", function () {
+    $(".container-formulario").css("display", "block");
+    accessForm = "new-login"
+    showFormOptions();
+})
+
+$("#accessNewAccount").on("click", function () {
+    $(".container-formulario").css("display", "block");
+    accessForm = "new-account"
+    showFormOptions();
+})
+
+$("#accessMyAccount").on("click", function () {
+    
+})
+
+$("#accessLogout").on("click", function () {
+
+})
