4266d0f0091f2de2e8ed1d5d6a4c2f68f102c079
Wendell Araujo <wendell170695@gmail.com>
2024-02-05 09:16:40 -0300

    criar várias contas

--- script/script.js
+++ script/script.js
@@ -17,14 +17,13 @@ function titleCase(str) {
 }
 
 function verificarLog() {
-  var logado = localStorage.getItem("user-log");
+  var logado = localStorage.getItem("log");
   var pageSelect = $(".page-select").text();
 
   if (logado !== null) {
     if (logado === "true") {
       $(".user-off").css("display", "none");
       $(".user-on").css("display", "block");
-      console.log("logado");
     } else {
       $(".user-off").css("display", "block");
       $(".user-on").css("display", "none");
@@ -34,21 +33,27 @@ function verificarLog() {
   } else {
     $(".user-off").css("display", "block");
     $(".user-on").css("display", "none");
-    localStorage.setItem("user-log", false);
+    localStorage.setItem("log", false);
     if (pageSelect === "minha-conta") window.location.replace("../index.html");
   }
 }
 
 function exibirNome() {
-  var logado = localStorage.getItem("user-log");
-  var nomeCompleto = localStorage.getItem("name");
-  var nome = "";
-  if (nomeCompleto !== null && nomeCompleto != undefined) {
-    nome = nomeCompleto.split(" ")[0];
-  }
+  var logado = localStorage.getItem("log");
   if (logado !== null) {
     if (logado === "true") {
-      $(".nome-boas-vindas").text(`Seja bem-vinda, ${nome}!`);
+      var userLogado = localStorage.getItem("user-log");
+      var usuarios = JSON.parse(localStorage.getItem("usuarios"));
+      var nomeCompleto = "";
+      var nome = "";
+
+     var usuarioLog = usuarios.usuarios[userLogado];
+
+      if (usuarioLog) {
+        nomeCompleto = usuarioLog.nome;
+        nome = nomeCompleto.split(" ")[0];
+        $(".nome-boas-vindas").text(`Seja bem-vinda, ${nome}!`);
+      }
     }
   }
 }
@@ -57,9 +62,11 @@ function showFormOptions() {
   if (accessForm === "new-login") {
     $(".new-account").css("display", "none");
     $(".new-login").css("display", "block");
+    $(".icon-label").css("display", "inline-block");
     $("#button-submit").text("Login");
   } else if (accessForm === "new-account") {
     $(".new-login").css("display", "none");
+    $(".icon-label").css("display", "none");
     $(".new-account").css("display", "block");
     $("#button-submit").text("Criar conta");
   }
@@ -80,10 +87,18 @@ function createNewAccount() {
   var password = $("#password-input").val();
   var passwordConfirm = $("#password-confirm-input").val();
   var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+  var usuarioSave = JSON.parse(localStorage.getItem("usuarios"));
 
   name = titleCase(name);
 
-  if (name === "") {
+  if (usuarioSave.usuarios.user) {
+    swal({
+      title: "Erro!",
+      text: "Usuário indisponível.",
+      icon: "error",
+      button: "OK",
+    });
+  } else if (name === "") {
     swal({
       title: "Erro!",
       text: "Preencha o campo nome.",
@@ -126,10 +141,22 @@ function createNewAccount() {
       button: "OK",
     });
   } else {
-    localStorage.setItem("name", name);
-    localStorage.setItem("email", email);
-    localStorage.setItem("user", user);
-    localStorage.setItem("password", password);
+    var usuario = {
+      nome: name,
+      email: email,
+      usuario: user,
+      senha: password,
+    };
+
+    var usuarios = { usuarios: {} };
+
+    if (usuarioSave) {
+      usuarioSave.usuarios[user] = usuario;
+      localStorage.setItem("usuarios", JSON.stringify(usuarioSave));
+    } else {
+      usuarios.usuarios[user] = usuario;
+      localStorage.setItem("usuarios", JSON.stringify(usuarios));
+    }
 
     swal({
       title: "Tudo certo!",
@@ -141,8 +168,8 @@ function createNewAccount() {
       closeOnEsc: false,
     });
     setTimeout(function () {
-      $("#user-input").val('');
-      $("#password-input").val('');
+      $("#user-input").val("");
+      $("#password-input").val("");
       $(".container-formulario").css("display", "block");
       accessForm = "new-login";
       showFormOptions();
@@ -153,19 +180,34 @@ function createNewAccount() {
 function login() {
   var userInput = $("#user-input").val();
   var passwordInput = $("#password-input").val();
-  var userSave = localStorage.getItem("user");
-  var passwordSave = localStorage.getItem("password");
+  var usuarios = JSON.parse(localStorage.getItem("usuarios"));
 
-  if (userInput !== userSave || passwordInput !== passwordSave) {
+  var usuarioLog = usuarios.usuarios[userInput]
+
+  if (usuarioLog && usuarioLog.senha === passwordInput) {
+    var nome = usuarioLog.nome;
+    nome = nome.split(" ")[0];
+    localStorage.setItem("log", true);
+    localStorage.setItem("user-log", userInput);
+    swal({
+      title: "Tudo certo!",
+      text: "Seja bem-vinda, " + nome + "!",
+      icon: "success",
+      buttons: false,
+      timer: 3000,
+      closeOnClickOutside: false,
+      closeOnEsc: false,
+    });
+    setTimeout(() => {
+      window.location.reload();
+    }, 3000);
+  } else {
     swal({
       title: "Erro!",
       text: "Usuário e/ou senha inválida.",
       icon: "error",
       button: "Tentar novamente",
     });
-  } else {
-    localStorage.setItem("user-log", true);
-    window.location.reload();
   }
 }
 
@@ -192,7 +234,8 @@ $("#accessNewAccount").on("click", function () {
 });
 
 $("#accessLogout").on("click", function () {
-  localStorage.setItem("user-log", false);
+  localStorage.setItem("log", false);
+  localStorage.setItem("user-log", "");
   swal({
     text: "Você saiu!",
     icon: "success",
